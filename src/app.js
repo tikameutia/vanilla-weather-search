926455625f16d0e2d4c6cc289c2e754431eccc0b
tikameutia <111536774+tikameutia@users.noreply.github.com>
2022-08-22 15:26:28 +0200

    Integrated with OpenWeather API

---
+++ src/app.js
@@ -0,0 +1,17 @@
+function showTemperature(response) {
+  document.querySelector("#city-name").innerHTML = response.data.name;
+  document.querySelector("#temp-today").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#weather-desc").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed * 3.6
+  );
+}
+
+let apiKey = "b32becf372227220ef6868c3037c0a49";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Bali&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
